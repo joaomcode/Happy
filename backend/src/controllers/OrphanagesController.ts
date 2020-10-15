348eae38243f475c77e8fe6b74b84b77b855ac46
joaomcode <joaomarcelo.co@hotmail.com>
2020-10-15 10:32:10 -0300

    Upload de imagens, trabalhando com views

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,14 +1,18 @@
 import { getRepository } from 'typeorm';
 import { Request, Response } from 'express';
+import orphanageView from '../views/orphanages_view';
 
-import Orphanage from '../models/Orphanage'
+import Orphanage from '../models/Orphanage';
 
 export default {
   async index(request: Request, response: Response) {
     const orphanagesRepository = getRepository(Orphanage);
 
-    const orphanages = await orphanagesRepository.find();
-    return response.json(orphanages);
+    const orphanages = await orphanagesRepository.find({
+      relations: ['images']
+    });
+
+    return response.json(orphanageView.renderMany(orphanages));
   },
 
   async show(request: Request, response: Response) {
@@ -16,9 +20,11 @@ export default {
 
     const orphanagesRepository = getRepository(Orphanage);
 
-    const orphanage = await orphanagesRepository.findOneOrFail(id);
+    const orphanage = await orphanagesRepository.findOneOrFail(id, {
+      relations: ['images']
+    });
 
-    return response.json(orphanage)
+    return response.json(orphanageView.render(orphanage));
   },
 
   async create(request: Request, response: Response) {
@@ -34,7 +40,13 @@ export default {
 
     const orphanagesRepository = getRepository(Orphanage);
 
-    const orphanage = orphanagesRepository.create({
+    const requestImages = request.files as Express.Multer.File[];
+
+    const images = requestImages.map(image => {
+      return { path: image.filename }
+    })
+
+    const data = {
       name,
       latitude,
       longitude,
@@ -42,10 +54,13 @@ export default {
       instructions,
       opening_hours,
       open_on_weekends,
-    });
+      images
+    };
+
+    const orphanage = orphanagesRepository.create(data);
 
     await orphanagesRepository.save(orphanage);
 
-    return response.status(201).json(orphanage)
+    return response.status(201).json(orphanage);
   }
 }
